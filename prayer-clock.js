e95d7bcfecb5029c4c8fc160f9e4f56ca03dedfa
Mohamad Sharizal Ibrahim <yoyomasterz@gmail.com>
2025-07-02 16:12:30 +0800

    Update prayer-clock.js
    missing ,

--- prayer-clock.js
+++ prayer-clock.js
@@ -2,7 +2,7 @@ let prayerTimes = {}; // Filled dynamically
 
 const prayerColors = {
   Fajr: "#00BFFF",
-  Dhuhr: "#B8860B"  // DarkGoldenRod
+  Dhuhr: "#B8860B",  // DarkGoldenRod
   Asr: "#FF8C00",
   Maghrib: "#DC143C",
   Isha: "#8A2BE2"
